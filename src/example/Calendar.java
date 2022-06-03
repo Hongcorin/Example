09d1db6c97ac61bf4328d91b7ca6311473eccdde
Dangmoo <wldbs2209@naver.com>
2022-06-03 16:24:22 +0900

    New Calendar

--- src/example/Calendar.java
+++ src/example/Calendar.java
@@ -2,50 +2,51 @@ package example;
 
 import java.util.*;
 
+import example.Calendar;
+
 public class Calendar {
 	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
-	}
-	
-	public void printSampleCalendar() {
-		
-		System.out.println("일  월  화  수  목  금  토");
-		System.out.println("----------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		
-	}
-	
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		
-		System.out.println("반복 횟수를 입력하세요!");
-		int replay = scan.nextInt();
-		
-		for(int i = 0; i < replay; i++)	{
-			
-			System.out.println("달을 입력하세요");
-			int month = scan.nextInt();	
-			
-				if(month < 0) {
-					System.out.println("Have a nice day!");	
-				}
-			System.out.printf("%d월은 %d일 까지 있습니다\n", month, cal.getMaxDaysOfMonth(month));
+	public boolean isleapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
 		}
-		System.out.println("good bye");
+		return false;
+	}
+	
+	public int getMaxDayOfMonth(int year,int month) {
+		if(isleapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		}
+		else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+	
+	public void printCalendar(int year,int month) {
 		
-			
-		cal.printSampleCalendar();
-		scan.close();
+		System.out.printf("    <<%4d년 %3d월>>\n",year,month);
+		System.out.println(" 일  월  화  수  목  금  토");
+		System.out.println("-----------------------");
+		
 
-
+		int maxDay = getMaxDayOfMonth(year,month);
+		
+		for(int i =1; i <= maxDay; i++) {
+			System.out.printf("%3d",i);
+			if(i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+//		System.out.println("29 30 31");
+		
 	}
 
 }
